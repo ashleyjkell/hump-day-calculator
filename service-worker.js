e722c681fbbc3f8420543e0985486f3fe88c6a09
Ashley Kell <ashleyjkell@gmail.com>
2025-07-28 20:20:58 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,22 @@
-const CACHE_NAME = 'lift-tools-cache-v1';
-const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon.png', '/app.js'];
+const CACHE_NAME = 'lift-weight-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
   );
 });

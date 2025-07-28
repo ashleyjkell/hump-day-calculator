1d6610c313c85862da92f44bafa79df663bd6589
Ashley Kell <ashleyjkell@gmail.com>
2025-07-28 20:12:17 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,12 @@
-// Service worker JS code
+const CACHE_NAME = 'lift-tools-cache-v1';
+const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon.png', '/app.js'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
